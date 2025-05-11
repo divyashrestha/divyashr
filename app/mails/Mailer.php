07863d71bf115cb36ed51de17a0491fc7a183ad8
Divya Shrestha <divyashrestha042@gmail.com>
2025-05-11 14:06:40 +0545

    updated the mailer

--- app/mails/Mailer.php
+++ app/mails/Mailer.php
@@ -16,11 +16,11 @@ class Mailer
             Company: {$data['comments']}
         ";;
         return BaseMail::sendMail(
-            tos: ['hello@divyashr.com.np', 'work@divyashrestha.com.np'],
+            tos: [['email' => 'hello@divyashr.com.np'], ['email' => 'work@divyashrestha.com.np']],
             subject: "Contact Form {$data['subject']}",
             body: $emailBody,
-            reply_tos: [$data['email']],
-            bccs:['work@divyashrestha.com.np',]
+            reply_tos: [['email' => $data['email']]],
+            bccs: [['email' => 'work@divyashrestha.com.np']]
         );
     }
 }
