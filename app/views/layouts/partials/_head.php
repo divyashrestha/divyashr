adc9c3bb264bb3beeb95265348406248c3acea2f
Divya Shrestha <divyashrestha042@gmail.com>
2025-05-11 10:53:23 +0545

    updated head

--- app/views/layouts/partials/_head.php
+++ app/views/layouts/partials/_head.php
@@ -10,7 +10,7 @@ use divyashrestha\Mvc\View;
 <?php $this->load_partial('layouts/partials','meta')?>
 <link rel="icon" href="<?php $this->load_asset('assets/icon/favicon.jpg')?>" type="image/jpg">
 <link rel="stylesheet" href="<?php $this->load_asset('assets/css/vendor.css')?>"/>
-<link href="<?php echo $this->load_asset('assets/css/settings.css')?>" rel="stylesheet">
+<link href="<?php $this->load_asset('assets/css/settings.css')?>" rel="stylesheet">
 <link href="<?php $this->load_asset('assets/css/custom.css')?>" rel="stylesheet">
 <link rel="stylesheet" href="<?php $this->load_asset('assets/css/justin-red.css')?>">
 <!-- Google Tag Manager -->
